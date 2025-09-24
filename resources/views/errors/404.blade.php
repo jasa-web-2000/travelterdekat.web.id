e8e86706519ed5efc3be2b73a1daa5fac35c7e47
dion-zebua <zebuadbless@gmail.com>
2025-09-24 19:19:35 +0700

    malas ngoding, paham 🖐

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('content')
+    <x-background-hero>
+        <h1>Halaman Tidak Ditemukan!</h1>
+    </x-background-hero>
+
+    <div class="my-space"></div>
+
+    <div class="my-container text-center">
+        <p>Maaf halaman yang anda buka tidak ditemukan!</p>
+    </div>
+@endsection
