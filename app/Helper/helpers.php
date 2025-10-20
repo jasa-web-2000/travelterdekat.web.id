dfbdc2eed2f3b6d8f141b9f235d4295059ab5a77
dion-zebua <zebuadbless@gmail.com>
2025-10-20 21:26:12 +0700

    malas ngoding, paham 🖐

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -80,7 +80,7 @@ if (! function_exists('validationPhoneNumber')) {
         } elseif ($kalimantan) {
             return '+62 857-9190-4615';
         } elseif ($sulawesi) {
-            return '+62 823-9643-6349';
+            return '+62 812-4440-8470';
             // return '+62 857-0518-0556';
             // return '+62 821-3087-7954';
         } else {
